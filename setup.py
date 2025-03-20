ab4a7011b8bb2c887ff26b8ddd3351bce8e2f05b
dbuffat <dimitri.buffat@etu.univ-lyon1.fr>
2025-03-20 09:37:41 +0100

    Packaging with uv

--- setup.py
+++ setup.py
@@ -1,28 +1,4 @@
 from setuptools import setup
 
-setup(
-    name="ghost_buster",
-    author="Dimitri Buffat",
-    author_email="dimitri.buffat@etu.univ-lyon1.fr",
-    url = "https://github.com/dbuffat/lsst_ghost_buster",
-    packages=["ghost_buster"],
-    description="Analysis of Rubin telescope ghost images",
-    setup_requires=['setuptools_scm'],
-    long_description=open("README.md").read(),
-    package_data={"": ["README.md"]},
-    include_package_data=True,
-    classifiers=[
-        "Development Status :: 1 - Beta",
-        "License :: OSI Approved :: GPL License",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        ],
-    install_requires=["batoid>=0.8",
-                      "matplotlib",
-                      "numpy",
-                      "pandas",
-                      "pyarrow",
-                      "setuptools_scm"]
-)
+setup()
+
